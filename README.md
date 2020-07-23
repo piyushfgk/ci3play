9036fb15da1adabe670ac4d051b436b1d491e127
Piyush Sachan <piyush@fgk.net>
2020-07-23 07:36:18 +0530

    Added README - First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CodeIgniter 3 Playground for Learning (ci3play.home.in)
+
+## History
+**3/07/2020 06:30 Hrs**
+- Created public to set it as document root thus enhancing security
+- Set full path name of application and system directory in public/index.php
+- Set base url & created salt for encryption key in application/config/config.php
+- Set default database to mysql, using database ci4 with prefix ci3_
+- Created default controller Pages
+- Created default method view for view
+- Created header and footer templates under views/templates
+- 
---
+++ application/controllers/Pages.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$data = array(
+			"page"		=> (object) ["title" => 'Home'],
+			"heading"	=> "Hi, Welcome to CI3 playground !"
+		);
+
+		$this->view('home', $data);
+	}
+
+	public function view($page = 'home', $data = array()){
+
+		if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+                // Whoops, we don't have a page for that!
+                show_404();
+		}
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+	}
+}
